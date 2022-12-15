070694196091a1dcd83b7ec9c7fa2dee2f9cab70
Henning Spier <henning.spier@gmail.com>
2022-12-15 10:32:56 +0100

    mit footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,19 +3,25 @@ export default function Footer(){
 
 
     return (
-        <footer>
-            <a href="" >Guidelines</a> | 
-            <a href="" >FAQ</a> | 
-            <a href="" >Lists</a> | 
-            <a href="" >API</a> | 
-            <a href="" >Security</a> | 
-            <a href="" >Legal</a> | 
-            <a href="" >Apply to YC</a> | 
-            <a href="" >Contact</a>
-            <form>
-                <span>Search:</span>
-                <input type="text" />
-            </form>
+        <footer id='inhalt'>
+            <div className="footer">
+                <div className="footer_links">
+                    <a href="" >Guidelines</a> | 
+                    <a href="" >FAQ</a> | 
+                    <a href="" >Lists</a> | 
+                    <a href="" >API</a> | 
+                    <a href="" >Security</a> | 
+                    <a href="" >Legal</a> | 
+                    <a href="" >Apply to YC</a> | 
+                    <a href="" >Contact</a>
+                </div>
+                <div>
+                    <form>
+                        <span>Search:</span>
+                        <input type="text" />
+                    </form>
+                </div>
+            </div>
         </footer>
     );
 }
