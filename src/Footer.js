daad6b6648c58334b60d91e430cda94e8a115769
Viola Kaemmer <v.kaemmer@gmx.de>
2022-12-16 10:52:58 +0100

    added search button in Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -19,6 +19,7 @@ export default function Footer(){
                     <form>
                         <span>Search:</span>
                         <input type="text" />
+                        <input type="submit" value="go!"/>
                     </form>
                 </div>
             </div>
